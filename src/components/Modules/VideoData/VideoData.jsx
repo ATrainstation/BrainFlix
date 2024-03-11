c5431e5d6f8709cd30fa384d8e312be90947f1d5
ATrainstation <aaronjtrainor@gmail.com>
2024-03-10 21:56:46 -0400

    finalize styling

--- src/components/Modules/VideoData/VideoData.jsx
+++ src/components/Modules/VideoData/VideoData.jsx
@@ -7,9 +7,34 @@ export function VideoData(props) {
 
 
       // Date conversion
-      const newDate = new Date(timestamp);
-      const options = { month: 'numeric', day: 'numeric', year: 'numeric' };
-      const formattedDate = newDate.toLocaleDateString('en-US', options);
+      function formatTimestamp(timestamp) {
+        const currentDate = new Date();
+        const commentDate = new Date(timestamp);
+    
+        const timeDifference = currentDate - commentDate;
+        const seconds = Math.floor(timeDifference / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(months / 12);
+    
+        if (months > 12) {
+          return `${years} year${years === 1 ? "" : "s"} ago`;
+        } else if (months >= 1) {
+          return `${months} month${months === 1 ? "" : "s"} ago`;
+        } else if (days > 0) {
+          return `${days} day${days === 1 ? "" : "s"} ago`;
+        } else if (hours > 0) {
+          return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+        } else if (minutes > 0) {
+          return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+        } else {
+          return "Just now";
+        }
+      }
+    
+      const formattedDate = formatTimestamp(timestamp);
 
   return (
 
