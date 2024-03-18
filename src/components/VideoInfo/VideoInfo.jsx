61ebbac455e5a1df3ecae38bbaf524821de09188
ATrainstation <aaronjtrainor@gmail.com>
2024-03-17 23:55:52 -0400

    remove unnecessary wrappers

--- src/components/VideoInfo/VideoInfo.jsx
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -5,13 +5,11 @@ import { VideoDesc } from "../Modules/VideoDesc/VideoDesc";
 
 export function VideoInfo({ selectedVideo }) {
   return (
-    <>
       <section className="video__cont__info">
         <HeaderText video={selectedVideo} />
         <VideoData video={selectedVideo} />
         <VideoDesc video={selectedVideo} />
       </section>
-    </>
   );
 }
 
